8810f97f59646bcae75fda3fd0fe34c39fd1fc42
mrsleep12 <galihalfikri86@gmail.com>
2025-08-27 02:01:46 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+const { startAgent } = require("wasp");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+startAgent("agent/target/wasm32-unknown-unknown/release/agent.wasm", async (input) => {
+  const askQuestion = () => {
+    rl.question("Ask your question: ", async (question) => {
+      const output = await input({ question });
+      console.log("Agent Response:", output.response);
+      console.log("History:", output.history.join(" | "));
+      askQuestion();
+    });
+  };
+  askQuestion();
+});
