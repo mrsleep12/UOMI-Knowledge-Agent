1a0d663ab18382f46daaf319c68e50348ca51baa
mrsleep12 <galihalfikri86@gmail.com>
2025-08-27 02:26:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const rl = readline.createInterface({
 
 startAgent("agent/target/wasm32-unknown-unknown/release/agent.wasm", async (input) => {
   const askQuestion = () => {
-    rl.question("Ask your question: ", async (question) => {
+    rl.question("Ask your question (or in Bahasa): ", async (question) => {
       const output = await input({ question });
       console.log("Agent Response:", output.response);
       console.log("History:", output.history.join(" | "));
